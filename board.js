0fc571d890eeb89d0f8b0b2c8ca1774ef82fe038
Yugank Singhal <s.yugank@gmail.com>
2019-10-27 02:03:18 -0400

    Basic game mechanics

--- board.js
+++ board.js
@@ -1,15 +1,38 @@
+const symbols= [0, 1, 2, 3, 4, 5, 6, 7]
+
 class GameBoard extends HTMLElement {
     constructor () {
         super();
         this.flipped = [];
-        this.cards = []
+        this.completed = [];
+        this.addEventListener('revealCard', e => this.handleCardReveal(e));
     }
 
     initGame() {
         for (let i=0; i<16; i++) {
             let newCard = document.createElement("game-card");
+            newCard.setAttribute("value", i%8)
             this.appendChild(newCard);
         }
+    }
+
+    handleCardReveal(e) {
+        if (this.completed.indexOf(e.detail.value) > -1) return;
+        this.flipped.push(e.detail.card);
+        if (this.flipped.length > 1) {
+            if (this.flipped[0].value === this.flipped[1].value) {
+                this.completed.push(this.flipped[0].value);
+                this.flipped.pop();
+                this.flipped.pop();
+            } else {
+                setTimeout(() => {
+                    this.flipped[0].flipClose();
+                    this.flipped[1].flipClose();
+                    this.flipped.pop();
+                    this.flipped.pop();
+                }, 1500);
+            }
+        } 
     }
 
     connectedCallback() {
--- card.js
+++ card.js
@@ -4,29 +4,43 @@ class GameCard extends HTMLElement {
         this.flip = false;
         this.root = null;
         this.value = null;
-        this.addEventListener('click', () => this.flipCard());
+        this.addEventListener('click', () => this.revealCard());
     }
     
     connectedCallback() {
+        this.value = this.getAttribute('value');
         this.root = document.createElement('div');
         let front = document.createElement('div');
         front.classList.add('front');
         let back = document.createElement('div')
         back.classList.add('back');
+        back.innerHTML = this.value;
         this.root.classList.add('card');
         this.root.appendChild(front);
         this.root.appendChild(back);
         this.appendChild(this.root);
     }
 
+    revealCard() {
+        this.flipOpen();
+        console.log('reveal');
+        this.dispatchEvent(new CustomEvent('revealCard', {
+            bubbles: true, 
+            detail: { 
+                value: this.value, 
+                card: this
+            }
+        }));
+    }
+
     flipOpen() {
         this.flip = true;
-        this.root.classList.add("flipped")
+        this.root.classList.add("flipped");
     }
     
     flipClose() {
         this.flip = false;
-        this.root.classList.remove("flipped")
+        this.root.classList.remove("flipped");
     }
 
     flipCard() {
